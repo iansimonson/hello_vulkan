4e780e734a53ead5fe7cf896c1d990b8abc522c2
Ian Simonson <ian@iansimonson.com>
2023-03-01 06:16:56 -0800

    Move triangle data to vertex buffers
    follows the vulkan tutorial except it uses two binding descriptors with
    an attribute each vs a single binding descriptor with multiple
    attributes. It means there are two vertex data arrays, one with
    positions and one with colors rather than the interleaved Vertex struct
    from the tutorial

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 C:\VulkanSDK\1.3.216.0\Bin\glslc.exe .\shaders\shader.vert -o .\shaders\vert.spv
 C:\VulkanSDK\1.3.216.0\Bin\glslc.exe .\shaders\shader.frag -o .\shaders\frag.spv
 odin build . -debug
-pause
